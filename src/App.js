271fa64c763a61667258019da6c1f9a67f6b73c5
RdzCore <daniel_rod_con@hotmail.com>
2021-06-14 15:54:50 -0500

    created note component a aded some states in app component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+import { Note } from "./components/Note";
+import "./App.css";
+
+function App(props) {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("");
+  const [showAll, setShowAll] = useState(true);
+
+  const handleChange = (e) => {
+    setNewNote(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newNoteToAdd = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: false,
+    };
+
+    setNotes(notes.concat(newNoteToAdd));
+    setNewNote("");
+  };
+  const handleShow = () => {
+    setShowAll(() => !showAll);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Notes</h1>
+      <button onClick={handleShow}>
+        {showAll ? "Show Importants" : "Show All"}
+      </button>
+      <ul>
+        {notes
+          .filter((note) => {
+            if (showAll === true) return true;
+            return note.important === true;
+          })
+          .map((note) => (
+            <Note key={note.id} {...note} />
+          ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} value={newNote} />
+        <button>Crate Note</button>
+      </form>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const notes = [
+  { id: 1, content: "Hello my friends", date: "14/06/2021", important: true },
+  { id: 2, content: "Hello from react", date: "14/06/2021", important: true },
+  { id: 3, content: "Todo this", date: "14/06/2021", important: false },
+];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App notes={notes} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
