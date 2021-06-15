7bb5c3e92863b9257e7c9837c118b3d4eebb7eb6
RdzCore <daniel_rod_con@hotmail.com>
2021-06-14 21:56:56 -0500

    create a const URL

--- src/services/notes/index.js
+++ src/services/notes/index.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
+const _URL = "http://localhost:3001/api/notes";
+
 export const getAllNotes = () => {
-  return axios.get("http://localhost:3001/api/notes").then((response) => {
+  return axios.get(_URL).then((response) => {
     const { data } = response;
     return data;
   });
 };
 
 export const createNewNote = ({ content }) => {
-  return axios
-    .post("http://localhost:3001/api/notes", { content })
-    .then((response) => {
-      const { data } = response;
-      return data;
-    });
+  return axios.post(_URL, { content }).then((response) => {
+    const { data } = response;
+    return data;
+  });
 };
